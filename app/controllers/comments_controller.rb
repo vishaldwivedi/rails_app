2e553caeb13a2d283c852db21940a40ead6b461d
Vishal Dwivedi <vishaldwivedi13@gmail.com>
2021-05-18 12:40:24 +0530

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
     def create
+        # creating a new comment
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
